90a02bd11fa9af0aa4f3c965a4df8c96e93e000a
Johnybonny <szympast@gmail.com>
2024-03-15 10:48:32 +0100

    Add first cycle generation

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -46,33 +46,56 @@ double euclideanDistance(const vector<int>& p1, const vector<int>& p2)
     return sqrt(sum);
 }
 
+int findFurthestPoint(const vector<vector<double>>& distances, const vector<bool>& taken, int point)
+{
+    double maximumDistance = distances[point][point];
+    int furthestPoint = point;
+    for (size_t j = 0; j < distances[point].size(); ++j)
+    {
+        if ((distances[point][j] > maximumDistance) && !taken[j])
+        {
+            maximumDistance = distances[point][j];
+            furthestPoint = j;
+        }
+    }
+    return furthestPoint;
+}
+
+int findClosestPoint(const vector<vector<double>>& distances, const vector<bool>& taken, int point)
+{
+    double minimumDistance = 1000000000; // TODO: Choose good number for minimum distance
+    int closestPoint = point;
+    for (size_t j = 0; j < distances[point].size(); ++j)
+    {
+        if ((distances[point][j] < minimumDistance) && !taken[j])
+        {
+            minimumDistance = distances[point][j];
+            closestPoint = j;
+        }
+    }
+    return closestPoint;
+}
+
 vector<vector<int>> initializeCycles(const vector<vector<double>>& distances)
 {
     // Choose starting point
     srand( time( NULL ) );
     int startingPoint = rand() % distances.size();
 
-    cout << "Wylosowany punkt: " << startingPoint << "\n";
-    cout << "Odleglosci:\n";
-    for (size_t i = 0; i < 10; ++i)
-    {
-        cout << distances[startingPoint][i] << "\n";
-    }
-
     // Choose second point
-    double maximumDistance = distances[startingPoint][startingPoint];
-    int secondPoint = startingPoint;
-    for (size_t j = 0; j < distances[startingPoint].size(); ++j)
-    {
-        if (distances[startingPoint][j] > maximumDistance)
-        {
-            maximumDistance = distances[startingPoint][j];
-            secondPoint = j;
-        }
-    }
+    int secondPoint = findFurthestPoint(distances, vector<bool>(distances.size(), false), startingPoint);
 
     vector<vector<int>> initializedCycles = {vector<int> {startingPoint}, vector<int> {secondPoint}};
     return initializedCycles;
+}
+
+void showCycles(const vector<vector<int>>& cycles)
+{
+    cout << "First cycle\tSecond cycle\n";
+    for (size_t j = 0; j < cycles[0].size(); ++j)
+    {
+        cout << cycles[0][j] << "\t\t" << cycles[1][j] << "\n";
+    }
 }
 
 int main(int argc, char* argv[])
@@ -99,10 +122,19 @@ int main(int argc, char* argv[])
 
     // Initialize cycles with first point in each
     vector<vector<int>> cycles = initializeCycles(distances);
-    cout << "First point: " << cycles[0][0] << "\n";
-    cout << "Second point: " << cycles[1][0] << "\n";
-    cout << "Distance between them: " << distances[cycles[0][0]][cycles[1][0]] << "\n";
+    vector<bool> taken = vector<bool>(distances.size(), false);
+    taken[cycles[0][0]] = 1;
+    taken[cycles[1][0]] = 1;
 
+    // Start the cycles with second point for each previously generated point
+    int newPoint0 = findClosestPoint(distances, taken, cycles[0][0]);
+    cycles[0].push_back(newPoint0);
+    taken[newPoint0] = true;
+    int newPoint1 = findClosestPoint(distances, taken, cycles[1][0]);
+    cycles[1].push_back(newPoint1);
+    taken[newPoint1] = true;
+
+    showCycles(cycles);
 
     return 0;
 }
