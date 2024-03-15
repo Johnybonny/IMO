fc1fad91b8dd0f495781c9c0ac95730ec4b44989
Johnybonny <szympast@gmail.com>
2024-03-15 08:27:10 +0100

    Refactor code

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -6,24 +6,28 @@
 
 using namespace std;
 
-vector<vector<int>> parseInput(const string& filename) {
+vector<vector<int>> parseInput(const string& filename)
+{
     vector<vector<int>> result;
     ifstream inputFile(filename);
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         cerr << "Failed to open file: " << filename << endl;
         return result;
     }
 
     string line;
-    while (getline(inputFile, line)) {
+    while (getline(inputFile, line))
+    {
         stringstream ss(line);
         vector<int> coordinates;
         int value;
         ss >> value; // Skip the first number
-        while (ss >> value) {
+        while (ss >> value)
+        {
             coordinates.push_back(value);
         }
-        if(coordinates.size() > 0) 
+        if(coordinates.size() > 0)
             result.push_back(coordinates);
     }
 
@@ -31,16 +35,20 @@ vector<vector<int>> parseInput(const string& filename) {
     return result;
 }
 
-double euclideanDistance(const vector<int>& p1, const vector<int>& p2) {
+double euclideanDistance(const vector<int>& p1, const vector<int>& p2)
+{
     double sum = 0.0;
-    for (size_t i = 0; i < p1.size(); ++i) {
+    for (size_t i = 0; i < p1.size(); ++i)
+    {
         sum += pow(p1[i] - p2[i], 2);
     }
     return sqrt(sum);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
         cerr << "Usage: " << argv[0] << " <input_filename>" << endl;
         return 1;
     }
@@ -50,18 +58,22 @@ int main(int argc, char* argv[]) {
 
     // Calculate Euclidean distances between each pair of vectors
     vector<vector<double>> distances(parsedData.size(), vector<double>(parsedData.size(), 0.0));
-    for (size_t i = 0; i < parsedData.size(); ++i) {
-        for (size_t j = i + 1; j < parsedData.size(); ++j) {
+    for (size_t i = 0; i < parsedData.size(); ++i)
+    {
+        for (size_t j = i + 1; j < parsedData.size(); ++j)
+        {
             double dist = euclideanDistance(parsedData[i], parsedData[j]);
             distances[i][j] = distances[j][i] = dist;
         }
     }
 
     // Output the distances
-        for (size_t j = 0; j < distances[0].size(); ++j) {
-            cout << distances[0][j] << " ";
-        }
-        cout << endl;
+    for (size_t j = 0; j < distances[0].size(); ++j)
+    {
+        cout << distances[0][j] << " ";
+    }
+    cout << endl;
+
 
     return 0;
 }
